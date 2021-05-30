912b4e980b789a43602f99761d956338d47c5db5
THANGNT <thangnt@winds.vn>
2021-05-30 12:39:43 +0700

    .

--- server/routes/category.js
+++ server/routes/category.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   var mysql = require("mysql");
   var connection = mysql.createConnection({
-    host: "0.0.0.0",
+    host: "13.212.199.143",
     user: "root",
     port: 5555,
     password: "123456Aa@",
